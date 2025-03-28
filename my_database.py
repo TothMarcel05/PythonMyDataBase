7a0360fb70691e096d8c748f7734fc92ad41821e
Tóth Marcel Dániel <toth.marcel.daniel@diak.szbi-pg.hu>
2025-03-28 11:22:24 +0100

    .

--- my_database.py
+++ my_database.py
@@ -3,12 +3,14 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="mysql"
+  password=""
 )
+
 
 mycursor = mydb.cursor()
 # általunk létrehozott adatbázis
-DATABASE = "mydatabase"
+DATABASE = "mydatabases"
+
 
 # táblázatok létrehozása
 mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {DATABASE}")
@@ -22,6 +24,42 @@ for x in mycursor:
 mycursor.execute(f"USE {DATABASE}")
 
 # táblázat létrehozása
-mycursor.execute("CREATE TABLE customers (name VARCHAR(255), addess VARCHAR(255))")
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
 
 # adatok beszúrása (INSERT)
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+]
+
+mycursor.executemany(sql, val)
+
+mydb.commit()
+
+#Select
+mycursor.execute("SELECT name, address FROM customers")
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
+  
+#Where
+sql = "SELECT * FROM customers WHERE address ='Park Lane 38'"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
